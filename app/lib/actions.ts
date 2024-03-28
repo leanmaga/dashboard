b1db971dfb334c21cbc74f92736167fa30dcec31
leanmaga <thesharkcommunity@gmail.com>
2024-03-28 20:32:56 -0300

    first commit

---
+++ app/lib/actions.ts
@@ -0,0 +1,48 @@
+'use server'
+
+// Marcar que todas las funciones que 
+//se exportan en este archivo son de servidor 
+//y por lo tanto no se ejecuta ni se envian 
+//al cliente
+
+import {z} from 'zod'
+import { sql } from '@vercel/postgres'
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
+
+const CreateInvoiceSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending','paid']),
+    date: z.string()
+})
+
+const CreateInvoiceFormSchema = CreateInvoiceSchema.omit({
+    id: true,
+    date: true
+})
+
+export async function createInvoice(formData: FormData){
+
+    const { customerId, amount, status } = CreateInvoiceFormSchema.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    })
+
+    //transformar errores de redondeo
+    const amountInCents = amount * 100
+
+    //creamos la fecha actual
+    const[date] = new Date().toISOString().split('T')
+
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`
+
+
+    revalidatePath('/dashboard/invoices/')
+    redirect('/dashboard/invoices/')
+}
